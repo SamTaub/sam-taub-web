83778a033db53714d751f1af9e2bfe3f80b2955d
SamTaub <staub91@gmail.com>
2019-06-20 15:12:59 -0400

    Changed resume to a download link, cleaned up inessential files

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,31 +2,46 @@ import React, { Component } from 'react';
 import './style.css';
 
 class About extends Component {
-
-    render() {
-        return (
-
-            <div className="container-fluid p-3" id="about">
-                <div className="row mt-5 mb-5 align-items-center">
-                    <div className="col-xs-12 col-md-6 d-flex justify-content-center mt-5">
-                        <img src={require('../../assets/images/aboutme.jpeg')} className="rounded-circle" alt="Sam" id="aboutmeimg" />
-                    </div>
-                    <div className="col-xs-12 col-md-6 mt-5">
-                        <p className="p-3" id="about-me-text">
-                            I'm a full stack web developer located in the Philadelphia area.  I enjoy coding every day and creating straightforward, easy to use web applications.  I prioritize clean code and mobile responsiveness in my applications.  Continue browsing my page to learn about the technologies I have used, sample projects, and how to connect with me.
-                        </p>
-                        <div className="row">
-                            <div className="col d-flex justify-content-center">
-                                <a href="//drive.google.com/file/d/18vyHRIT8Ueb30NIu19KbIKrAgIL1SJTM/view?usp=sharing" target="_blank"><button type="button" className="btn rounded-pill"><h5 className="p-1"> View Resume</h5></button></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="container-fluid p-3" id="about">
+        <div className="row mt-5 mb-5 align-items-center">
+          <div className="col-xs-12 col-md-6 d-flex justify-content-center mt-5">
+            <img
+              src={require('../../assets/images/aboutme.jpeg')}
+              className="rounded-circle"
+              alt="Sam"
+              id="aboutmeimg"
+            />
+          </div>
+          <div className="col-xs-12 col-md-6 mt-5">
+            <p className="p-3" id="about-me-text">
+              I'm a full stack web developer located in the Philadelphia area. I
+              enjoy coding every day and creating straightforward, easy to use
+              web applications. I prioritize clean code and mobile
+              responsiveness in my applications. Continue browsing my page to
+              learn about the technologies I have used, sample projects, and how
+              to connect with me.
+            </p>
+            <div className="row">
+              <div className="col d-flex justify-content-center">
+                <a
+                  href={require('../../assets/TaubResume.pdf')}
+                  download="TaubResume"
+                >
+                  <button type="button" className="btn rounded-pill">
+                    <h5 className="p-1">
+                      <i class="fa fa-download" /> Resume
+                    </h5>
+                  </button>
+                </a>
+              </div>
             </div>
-
-        );
-    }
-
-};
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default About;
