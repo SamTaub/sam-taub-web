6d9a7b32b737239d64fe97c1dbc09f18a92a97a2
SamTaub <staub91@gmail.com>
2019-06-21 11:51:51 -0400

    Updated about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,11 +16,22 @@ const About = () => {
         <div className="col-xs-12 col-md-6 mt-5">
           <p className="p-3" id="about-me-text">
             I'm a full stack web developer located in the Philadelphia area. I
-            enjoy coding every day and creating straightforward, easy to use web
-            applications. I prioritize clean code and mobile responsiveness in
-            my applications. Continue browsing my page to learn about the
-            technologies I have used, sample projects, and how to connect with
-            me.
+            have a passion for coding and the technology industry. I began
+            learning to develop websites during the summer of 2018 and completed
+            University of Pennsylvania's LPS Coding Bootcamp in May 2019. I
+            prioritize clean code, mobile responsiveness, and utilizing React.
+            <br />
+            <br />
+            Currently I am focused on advancing my knowledge of React by
+            learning to utilize Redux and Hooks. I am also learning advanced CSS
+            3 and Sass. I plan to have a new project up demonstrating these new
+            skills and technologies in the next couple weeks.
+            <br />
+            <br />
+            Outside of coding and technology, I am usually found consuming
+            anything related to NFL Football - games, documentaries, podcasts,
+            analytics, fantasy and everything else. I am the co-commissioner of
+            a home fantasy league that is entering it's 5th year.
           </p>
           <div className="row">
             <div className="col d-flex justify-content-center">
