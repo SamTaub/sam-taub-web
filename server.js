306d835bb54b1f5c34441932fd3fc89dafab4f78
SamTaub <staub91@gmail.com>
2019-06-21 10:40:38 -0400

    Cleaned up unnecessary comments

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Static Assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
